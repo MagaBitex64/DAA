6da6fab6d68ab21bdf961cda1878c5b4f78ff1e4
MagaBitex64 <maqa25125@gmail.com>
2025-09-18 16:37:57 +0500

    Create AppTest.java

---
+++ src/test/java/com/algos/AppTest.java
@@ -0,0 +1,11 @@
+package com.algos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void basicTest() {
+        assertTrue(true, "First test should always pass!");
+    }
+}
